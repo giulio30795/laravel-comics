d70eb5c1abc9969829e8f0ffbd552dcad4f3d7be
giulio30795 <giulioincarbone95@gmail.com>
2022-01-19 20:13:07 +0100

    card-container

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,17 @@
     @extends('layout.layout')
 
     @section('main-content')
-        <h2>home</h2>
+    <main class="card-container">
+
+        <div class="container">
+            @foreach ($comics as $comic )
+                <div class="card">
+                    <div class="img-container">
+                        <img src="{{ $comic['thumb'] }} " alt="{{ $comic['series'] }}">
+                    </div>
+                    <h6>{{$comic['series']}}</h6>
+                </div>
+            @endforeach
+        </div>
+    </main>
     @endsection
