0fdeff8b0b32c6b0cd117d7ca5620d98a3e9a564
giulio30795 <giulioincarbone95@gmail.com>
2022-01-19 23:49:05 +0100

    footer style

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,34 @@
 <footer>
-    footer
+    <div class="container">
+            <div>
+                <div class="btn">
+                    sign-up now!
+                </div>
+            </div>
+            <div class="d-flex">
+                <div class="follow">
+                    Follow Us
+                </div>
+
+                <div>
+                    <ul class="d-flex">
+                        <li>
+                            <img src="{{ asset('images/footer-facebook.png') }}" alt="">
+                        </li>
+                        <li>
+                            <img src="{{ asset('images/footer-twitter.png') }}" alt="">
+                        </li>
+                        <li>
+                            <img src="{{ asset('images/footer-youtube.png') }}" alt="">
+                        </li>
+                        <li>
+                            <img src="{{ asset('images/footer-pinterest.png') }}" alt="">
+                        </li>
+                        <li>
+                            <img src="{{ asset('images/footer-periscope.png') }}" alt="">
+                        </li>
+                    </ul>
+                </div>
+            </div>
+    </div>
 </footer>
