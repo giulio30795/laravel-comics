71c2596aa1f8506cbf6fd6ff0d3e076b417dd178
giulio30795 <giulioincarbone95@gmail.com>
2022-01-19 16:02:58 +0100

    check

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,6 +2,6 @@
     <a href="{{ route ('home') }}">
         <img src="{{ asset('images/dc-logo.png') }}" alt="">
     </a>
-    <a href="{{ route ('home') }}">Comics</a>
-    <a href="{{ route ('news') }}">News</a>
+    <a class="link" href="{{ route ('home') }}">Comics</a>
+    <a class="link" href="{{ route ('news') }}">News</a>
 </header>
