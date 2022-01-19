ee6c52cf268b5b0448b929bb5a751df737cd3c8b
giulio30795 <giulioincarbone95@gmail.com>
2022-01-20 00:10:25 +0100

    check hover and active

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,7 +3,10 @@
         <a href="{{ route ('home') }}">
             <img src="{{ asset('images/dc-logo.png') }}" alt="">
         </a>
-        <a class="link" href="{{ route ('home') }}">Comics</a>
-        <a class="link" href="{{ route ('news') }}">News</a>
+        <a class=" @if(Request::route()->getName() === 'home')
+            active @endif link" href="{{ route ('home') }}">Comics</a>
+
+        <a class=" @if(Request::route()->getName() === 'news')
+            active @endif link" href="{{ route ('news') }}">News</a>
     </div>
 </header>
