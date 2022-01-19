9d9454635baec6bda6a600931b96d607245fab5a
giulio30795 <giulioincarbone95@gmail.com>
2022-01-19 19:26:33 +0100

    inizio header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,9 @@
 <header>
-    <a href="{{ route ('home') }}">
-        <img src="{{ asset('images/dc-logo.png') }}" alt="">
-    </a>
-    <a class="link" href="{{ route ('home') }}">Comics</a>
-    <a class="link" href="{{ route ('news') }}">News</a>
+    <div class="container">
+        <a href="{{ route ('home') }}">
+            <img src="{{ asset('images/dc-logo.png') }}" alt="">
+        </a>
+        <a class="link" href="{{ route ('home') }}">Comics</a>
+        <a class="link" href="{{ route ('news') }}">News</a>
+    </div>
 </header>
