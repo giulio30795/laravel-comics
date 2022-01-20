fe83695bf52396121f0665d6f781167d470879e2
giulio30795 <giulioincarbone95@gmail.com>
2022-01-20 16:29:07 +0100

    comic detail style done

--- resources/views/comic-detail.blade.php
+++ resources/views/comic-detail.blade.php
@@ -5,27 +5,37 @@
         <div class="jumbo">
             <div class="container">
                 <div class="comic-container">
-                    <img src="{{ $comic['thumb'] }}" alt="{{ $comic['series'] }}">
+                    <div class="img-container">
+                        <img src="{{ $comic['thumb'] }}" alt="{{ $comic['series'] }}">
+                        <span class="comic">COMIC BOOK</span>
+                        <span class="gallery">VIEW GALLERY</span>
+                    </div>
                 </div>
             </div>
         </div>
 
         <div class="container d-flex comic-description">
-            <div>
+            <div class="description">
                 <h3>{{ $comic['title'] }}</h3>
                 <div class="available">
-                    <div>
-                        <span>U.S Price:</span>
-                        {{ $comic['price'] }}
+                    <div class="d-flex">
+                        <div>
+                            <span>U.S Price:</span>
+                            {{ $comic['price'] }}
+                        </div>
+                        <div class="availability">
+                            Available
+                        </div>
                     </div>
+
                     <div>
-                        Available
+                        check Availability &#9660;
                     </div>
                 </div>
                 <p>{{ $comic['description'] }}</p>
             </div>
             <div class="adv">
-                <img src="{{ asset('/images/adv.jpg') }}" alt="ADV">
+                <img src="{{ $comic['thumb'] }}" alt="{{ $comic['series'] }}" alt="ADV">
             </div>
         </div>
     </main>
